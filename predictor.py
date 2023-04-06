c7a9896f069f39efd0f7b77d556614e23c06f4b5
Phodal Huang <h@phodal.com>
2023-04-06 19:47:21 +0800

    docs: init projects

---
+++ predictor.py
@@ -0,0 +1,14 @@
+import openbayes_serving as serv
+
+
+class Predictor:
+    def __init__(self):
+        pass
+
+
+    def predict(self, json):
+        pass
+
+
+if __name__ == '__main__':
+    serv.run(Predictor)
