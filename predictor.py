a394cbae4ab06e17d2e79d02d7927234908533fe
Phodal Huang <h@phodal.com>
2023-04-09 14:29:25 +0800

    docs: add app.py

--- predictor.py
+++ predictor.py
@@ -1,14 +1,11 @@
-import openbayes_serving as serv
-
-
-class Predictor:
-    def __init__(self):
-        pass
-
-
-    def predict(self, json):
-        pass
-
-
-if __name__ == '__main__':
-    serv.run(Predictor)
+# class Predictor:
+#     def __init__(self):
+#         pass
+#
+#
+#     def predict(self, json):
+#         pass
+#
+#
+# if __name__ == '__main__':
+#     serv.run(Predictor)
